60eaf4a7e86f59a7bd395bf4826d5a894ea09d90
sanjithT <20pw32@psgtech.ac.in>
2022-01-24 07:52:31 +0530

    h

--- src/components/Home.js
+++ src/components/Home.js
@@ -83,7 +83,7 @@ function Home() {
 export default Home
 
 const Container = styled.div`
-    height:100vh;
+    height:100%;
     overflow: scroll;
     overflow-x: hidden;
     scroll-snap-type: y mandatory;
